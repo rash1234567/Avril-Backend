8421a62bd1a8f5f5e7458bfc10fd1a108b8120f5
rash1234567 <imamrasheedatahmad1993@gmail.com>
2023-05-18 09:45:10 +0100

    new commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./src/.env" });
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
